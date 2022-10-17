bf06b88dc2b6b24587f3f62fd4700eed07b57852
diabhey <abhimanyuselvan@protonmail.com>
2022-10-17 14:24:04 +0200

    chore: cleanup/workflow

--- scripts/build_versions.sh
+++
@@ -1,6 +0,0 @@
-
-for V in 0.0.3 0.0.4 0.0.5 0.0.6
-do
-    pushd pkg && docker buildx build --build-arg=VERSION=$V --platform linux/amd64,linux/arm64,linux/arm . -t cnskunkworks/bots:$V --push && popd
-done
-
