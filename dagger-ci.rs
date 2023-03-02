6653bd2322db3b88267bf0ca63fdf56cd28ff3d9
diabhey <abhimanyuselvan@protonmail.com>
2023-03-02 15:20:31 +0100

    chore: ci cleanup

--- dagger-ci.rs
+++ dagger-ci.rs
@@ -1,17 +1,16 @@
+// Uses https://crates.io/crates/dagger-sdk for ci
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
-
     let client = dagger_sdk::connect().await?;
-    let context_dir = client
-        .host()
-        .directory("./pkg/app");
+    let context_dir = client.host().directory("./pkg/app");
 
     let _reg = client
         .container()
         .build(context_dir.id().await?)
-        // .publish(format!("docker.io/abigillu/bots-dagger"))
-        .publish(format!("registry.digitalocean.com/diabhey/hivenetes/bots:dagger"))
+        .publish(format!(
+            "registry.digitalocean.com/diabhey/hivenetes/bots:dagger"
+        ))
         .await?;
 
     println!("Published image to: {:?}", _reg);
