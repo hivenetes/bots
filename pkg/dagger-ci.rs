503ef55d7734f9dd2a85ccaeebe6efdc993cc211
diabhey <abhimanyuselvan@protonmail.com>
2023-03-02 11:45:35 +0100

    feat: inc dagher-sdk.rs

---
+++ pkg/dagger-ci.rs
@@ -0,0 +1,19 @@
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+
+    let client = dagger_sdk::connect().await?;
+    let context_dir = client
+        .host()
+        .directory("./app");
+
+    let _reg = client
+        .container()
+        .build(context_dir.id().await?)
+        .publish(format!("docker.io/abigillu/bots-dagger"))
+        .await?;
+
+    println!("Published image to: {:?}", _reg);
+
+    Ok(())
+}
